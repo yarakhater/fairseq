ecc055ea4d8cc07f291a14605b4cc5d93a7b24be
yarakhater <yarakhater-21@outlook.com>
2022-05-24 13:43:03 +0200

    2014 folder

--- default_train.sh
+++ default_train.sh
@@ -5,4 +5,4 @@
 #SBATCH --gres=gpu:1
 #SBATCH --time=12:00:00
 #SBATCH --ntasks=1
-CUDA_VISIBLE_DEVICES=0 python train.py  data-bin/iwslt14.tokenized.de-en --lr 0.25 --optimizer nag --clip-norm 0.1  --dropout 0.2 --max-tokens 4000 --max-sentences 64 --arch fconv_iwslt_de_en --save-dir checkpoints/fconv
+python train.py  data-bin/iwslt14.tokenized.de-en --lr 0.25 --optimizer nag --clip-norm 0.1  --dropout 0.2 --max-tokens 4000 --max-sentences 64 --arch fconv_iwslt_de_en --save-dir checkpoints/fconv
